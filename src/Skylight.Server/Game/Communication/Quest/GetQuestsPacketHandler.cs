6050c9be775a60b9bc7385c8a797c28b4c03cac7
Oliver <jasper.kontturi@gmail.com>
2023-01-27 09:56:55 +0200

    protocol whore

---
+++ src/Skylight.Server/Game/Communication/Quest/GetQuestsPacketHandler.cs
@@ -0,0 +1,18 @@
+﻿using Net.Communication.Attributes;
+using Skylight.API.Game.Users;
+using Skylight.Protocol.Packets.Data.Quest;
+using Skylight.Protocol.Packets.Incoming.Quest;
+using Skylight.Protocol.Packets.Manager;
+using Skylight.Protocol.Packets.Outgoing.Quest;
+
+namespace Skylight.Server.Game.Communication.Quest;
+
+[PacketManagerRegister(typeof(AbstractGamePacketManager))]
+internal sealed class GetQuestsPacketHandler<T> : UserPacketHandler<T>
+	where T : IGetQuestsIncomingPacket
+{
+	internal override void Handle(IUser user, in T packet)
+	{
+		//user.SendAsync(new QuestCompletedOutgoingPacket(new List<QuestMessageData>(), true));
+	}
+}
